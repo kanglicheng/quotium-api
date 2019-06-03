8e074ab26bc247a3e5452fa7cd31bdc21bf4810d
kanglicheng <11033065+kanglicheng@users.noreply.github.com>
2019-06-03 01:13:04 -0500

    copied

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask
-app = Flask(__name__)
+import flask
+import json
+from flask import abort, request, jsonify
+from data_validation import PropertySubmissionSchema
+from marshmallow import Schema, fields, INCLUDE, pprint, ValidationError
+from calculate_estimates import RentalEstimate
 
-@app.route('/', methods=["GET"])
-def hello_world():
-    return 'Hello World!'
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+property_schema = PropertySubmissionSchema()
+
+#@app.route('/', methods=['GET'])
+@app.route('/')
+def home():
+    return "This is a test"
 
 if __name__ == '__main__':
     app.run()
