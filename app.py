65d307f27e08749ac32e2e32d2f251a5cd2dbfab
kanglicheng <11033065+kanglicheng@users.noreply.github.com>
2019-06-03 01:38:43 -0500

    more

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import json
 from flask import Flask
 from marshmallow import Schema, fields, INCLUDE, pprint, ValidationError
 from flask import abort, request, jsonify
@@ -15,6 +16,21 @@ def post_test():
         abort(400)
     else:
         return jsonify(request.json)
+
+@app.route('/submit', methods=['POST'])
+def validate_post_data():
+    if not request.json:
+        abort(400)
+    try:
+        # pprint(request.json)
+        # return request.json
+        a = property_schema.load(request.json)
+        estimate = process_property_data(a)
+        a['estimate'] = estimate
+        return jsonify(a)
+        #return jsonify(request.json), 201
+    except ValidationError as error:
+        return jsonify(error.messages), 422
 
 if __name__ == '__main__':
     app.run()
