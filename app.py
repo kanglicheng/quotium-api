48473f191b4792150baa7fa89d5d29341032d70d
kanglicheng <11033065+kanglicheng@users.noreply.github.com>
2019-06-03 01:23:30 -0500

    debug

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import flask
 import json
 from flask import Flask, abort, request, jsonify
 #from data_validation import PropertySubmissionSchema
-from marshmallow import Schema, fields, INCLUDE, pprint, ValidationError
+#from marshmallow import Schema, fields, INCLUDE, pprint, ValidationError
 #from calculate_estimates import RentalEstimate
 
 APP = Flask(__name__)
