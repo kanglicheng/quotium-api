53cb09a998d353c4ae2c3264269dee5f260a5f7b
kanglicheng <11033065+kanglicheng@users.noreply.github.com>
2019-06-03 01:31:26 -0500

    add a post route

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask
 from marshmallow import Schema, fields, INCLUDE, pprint, ValidationError
+from flask import abort, request, jsonify
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
     return 'Hello World!'
+
+@app.route('/test', methods=['POST'])
+def post_test():
+    if not request.json:
+        abort(400)
+    else:
+        return jsonify(request.json)
 
 if __name__ == '__main__':
     app.run()
